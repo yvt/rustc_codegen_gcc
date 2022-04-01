138a187fe270b018cf847ed6c1e66ed375602422
yvt <i@yvt.jp>
2022-04-01 12:37:50 +0900

    test: Test checked arithmetic
    - `{wrapping, checked}_{shl, shr}`
    - `{wrapping, checked, overflowing, saturating}_{add, sub, mul, div}`

--- tests/run/int.rs
+++ tests/run/int.rs
@@ -101,6 +101,24 @@ fn main(argc: isize, _argv: *const *const u8) -> isize {
                 check!($ty, $var3 << (argc + 63) as $ty);
             }
 
+            check!($ty, $var.wrapping_shl(argc as u32 - 1));
+            check!($ty, $var.wrapping_shl(argc as u32));
+            check!($ty, $var.wrapping_shl((argc + 32) as u32));
+            check!($ty, $var.wrapping_shl((argc + 48) as u32));
+            check!($ty, $var.wrapping_shl((argc + 60) as u32));
+            check!($ty, $var.wrapping_shl((argc + 62) as u32));
+            check!($ty, $var.wrapping_shl((argc + 63) as u32));
+            check!($ty, $var.wrapping_shl((argc + 80) as u32));
+
+            check!(Option<$ty>, $var.checked_shl(argc as u32 - 1));
+            check!(Option<$ty>, $var.checked_shl(argc as u32));
+            check!(Option<$ty>, $var.checked_shl((argc + 32) as u32));
+            check!(Option<$ty>, $var.checked_shl((argc + 48) as u32));
+            check!(Option<$ty>, $var.checked_shl((argc + 60) as u32));
+            check!(Option<$ty>, $var.checked_shl((argc + 62) as u32));
+            check!(Option<$ty>, $var.checked_shl((argc + 63) as u32));
+            check!(Option<$ty>, $var.checked_shl((argc + 80) as u32));
+
             check!($ty, $var >> (argc as $ty - 1));
             check!($ty, $var >> argc as $ty);
             if_ty! {
@@ -144,6 +162,24 @@ fn main(argc: isize, _argv: *const *const u8) -> isize {
                 check!($ty, $var3 >> (argc + 80) as $ty);
             }
 
+            check!($ty, $var.wrapping_shr(argc as u32 - 1));
+            check!($ty, $var.wrapping_shr(argc as u32));
+            check!($ty, $var.wrapping_shr((argc + 32) as u32));
+            check!($ty, $var.wrapping_shr((argc + 48) as u32));
+            check!($ty, $var.wrapping_shr((argc + 60) as u32));
+            check!($ty, $var.wrapping_shr((argc + 62) as u32));
+            check!($ty, $var.wrapping_shr((argc + 63) as u32));
+            check!($ty, $var.wrapping_shr((argc + 80) as u32));
+
+            check!(Option<$ty>, $var.checked_shr(argc as u32 - 1));
+            check!(Option<$ty>, $var.checked_shr(argc as u32));
+            check!(Option<$ty>, $var.checked_shr((argc + 32) as u32));
+            check!(Option<$ty>, $var.checked_shr((argc + 48) as u32));
+            check!(Option<$ty>, $var.checked_shr((argc + 60) as u32));
+            check!(Option<$ty>, $var.checked_shr((argc + 62) as u32));
+            check!(Option<$ty>, $var.checked_shr((argc + 63) as u32));
+            check!(Option<$ty>, $var.checked_shr((argc + 80) as u32));
+
             // Casts
             if_ty! {
                 u64 | i64 | u128 | i128 =>
@@ -153,31 +189,95 @@ fn main(argc: isize, _argv: *const *const u8) -> isize {
 
             // Addition.
             check!($ty, $var + argc as $ty);
-
             check!($ty, $var2 + argc as $ty);
             check!($ty, $var2 + ($var2 + argc as $ty) as $ty);
-
             check!($ty, $var3 + argc as $ty);
+
+            check!(Option<$ty>, $var.checked_add(argc as $ty));
+            check!(Option<$ty>, $var2.checked_add(argc as $ty));
+            check!(Option<$ty>, $var2.checked_add(($var2 + argc as $ty) as $ty));
+            check!(Option<$ty>, $var3.checked_add($ty::MAX));
+            check!(Option<$ty>, $var3.checked_add($ty::MIN));
+
+            check!($ty, $var.wrapping_add(argc as $ty));
+            check!($ty, $var2.wrapping_add(argc as $ty));
+            check!($ty, $var2.wrapping_add(($var2 + argc as $ty) as $ty));
+            check!($ty, $var3.wrapping_add($ty::MAX));
+            check!($ty, $var3.wrapping_add($ty::MIN));
+
+            check!(($ty, bool), $var.overflowing_add(argc as $ty));
+            check!(($ty, bool), $var2.overflowing_add(argc as $ty));
+            check!(($ty, bool), $var2.overflowing_add(($var2 + argc as $ty) as $ty));
+            check!(($ty, bool), $var3.overflowing_add($ty::MAX));
+            check!(($ty, bool), $var3.overflowing_add($ty::MIN));
+
+            check!($ty, $var.saturating_add(argc as $ty));
+            check!($ty, $var2.saturating_add(argc as $ty));
+            check!($ty, $var2.saturating_add(($var2 + argc as $ty) as $ty));
+            check!($ty, $var3.saturating_add($ty::MAX));
+            check!($ty, $var3.saturating_add($ty::MIN));
 
             // Subtraction
             check!($ty, $var - argc as $ty);
-
             check!($ty, $var2 - argc as $ty);
-
             check!($ty, $var3 - argc as $ty);
+
+            check!(Option<$ty>, $var.checked_sub(argc as $ty));
+            check!(Option<$ty>, $var2.checked_sub(argc as $ty));
+            check!(Option<$ty>, $var2.checked_sub(($var2 + argc as $ty) as $ty));
+            check!(Option<$ty>, $var3.checked_sub($ty::MAX));
+            check!(Option<$ty>, $var3.checked_sub($ty::MIN));
+
+            check!($ty, $var.wrapping_sub(argc as $ty));
+            check!($ty, $var2.wrapping_sub(argc as $ty));
+            check!($ty, $var2.wrapping_sub(($var2 + argc as $ty) as $ty));
+            check!($ty, $var3.wrapping_sub($ty::MAX));
+            check!($ty, $var3.wrapping_sub($ty::MIN));
+
+            check!(($ty, bool), $var.overflowing_sub(argc as $ty));
+            check!(($ty, bool), $var2.overflowing_sub(argc as $ty));
+            check!(($ty, bool), $var2.overflowing_sub(($var2 + argc as $ty) as $ty));
+            check!(($ty, bool), $var3.overflowing_sub($ty::MAX));
+            check!(($ty, bool), $var3.overflowing_sub($ty::MIN));
+
+            check!($ty, $var.saturating_sub(argc as $ty));
+            check!($ty, $var2.saturating_sub(argc as $ty));
+            check!($ty, $var2.saturating_sub(($var2 + argc as $ty) as $ty));
+            check!($ty, $var3.saturating_sub($ty::MAX));
+            check!($ty, $var3.saturating_sub($ty::MIN));
 
             // Multiplication
             check!($ty, $var * (argc + 1) as $ty);
             check!($ty, $var * (argc as $ty + $var2));
-
             check!($ty, $var2 * (argc + 1) as $ty);
             check!($ty, $var2 * (argc as $ty + $var2));
-
             check!($ty, $var3 * argc as $ty);
-
             check!($ty, $var4 * (argc + 1) as $ty);
-
             check!($ty, $var5 * (argc + 1) as $ty);
+
+            check!(Option<$ty>, $var.checked_mul((argc + 1) as $ty));
+            check!(Option<$ty>, $var.checked_mul((argc as $ty + $var2)));
+            check!(Option<$ty>, $var3.checked_mul($var3));
+            check!(Option<$ty>, $var4.checked_mul((argc + 1) as $ty));
+            check!(Option<$ty>, $var5.checked_mul((argc + 1) as $ty));
+
+            check!($ty, $var.wrapping_mul((argc + 1) as $ty));
+            check!($ty, $var.wrapping_mul((argc as $ty + $var2)));
+            check!($ty, $var3.wrapping_mul($var3));
+            check!($ty, $var4.wrapping_mul((argc + 1) as $ty));
+            check!($ty, $var5.wrapping_mul((argc + 1) as $ty));
+
+            check!(($ty, bool), $var.overflowing_mul((argc + 1) as $ty));
+            check!(($ty, bool), $var.overflowing_mul((argc as $ty + $var2)));
+            check!(($ty, bool), $var3.overflowing_mul($var3));
+            check!(($ty, bool), $var4.overflowing_mul((argc + 1) as $ty));
+            check!(($ty, bool), $var5.overflowing_mul((argc + 1) as $ty));
+
+            check!($ty, $var.saturating_mul((argc + 1) as $ty));
+            check!($ty, $var.saturating_mul((argc as $ty + $var2)));
+            check!($ty, $var3.saturating_mul($var3));
+            check!($ty, $var4.saturating_mul((argc + 1) as $ty));
+            check!($ty, $var5.saturating_mul((argc + 1) as $ty));
 
             // Division.
             check!($ty, $var / (argc + 1) as $ty);
@@ -193,6 +293,35 @@ fn main(argc: isize, _argv: *const *const u8) -> isize {
 
             check!($ty, $var4 / (argc + 1) as $ty);
             check!($ty, $var4 / (argc + 2) as $ty);
+
+            check!(Option<$ty>, $var.checked_div((argc + 1) as $ty));
+            check!(Option<$ty>, $var.checked_div((argc as $ty + $var2)));
+            check!(Option<$ty>, $var3.checked_div($var3));
+            check!(Option<$ty>, $var4.checked_div((argc + 1) as $ty));
+            check!(Option<$ty>, $var5.checked_div((argc + 1) as $ty));
+            check!(Option<$ty>, ($ty::MIN).checked_div((0 as $ty).wrapping_sub(argc as $ty)));
+            check!(Option<$ty>, $var5.checked_div((argc - 1) as $ty)); // var5 / 0
+
+            check!($ty, $var.wrapping_div((argc + 1) as $ty));
+            check!($ty, $var.wrapping_div((argc as $ty + $var2)));
+            check!($ty, $var3.wrapping_div($var3));
+            check!($ty, $var4.wrapping_div((argc + 1) as $ty));
+            check!($ty, $var5.wrapping_div((argc + 1) as $ty));
+            check!($ty, ($ty::MIN).wrapping_div((0 as $ty).wrapping_sub(argc as $ty)));
+
+            check!(($ty, bool), $var.overflowing_div((argc + 1) as $ty));
+            check!(($ty, bool), $var.overflowing_div((argc as $ty + $var2)));
+            check!(($ty, bool), $var3.overflowing_div($var3));
+            check!(($ty, bool), $var4.overflowing_div((argc + 1) as $ty));
+            check!(($ty, bool), $var5.overflowing_div((argc + 1) as $ty));
+            check!(($ty, bool), ($ty::MIN).overflowing_div((0 as $ty).wrapping_sub(argc as $ty)));
+
+            check!($ty, $var.saturating_div((argc + 1) as $ty));
+            check!($ty, $var.saturating_div((argc as $ty + $var2)));
+            check!($ty, $var3.saturating_div($var3));
+            check!($ty, $var4.saturating_div((argc + 1) as $ty));
+            check!($ty, $var5.saturating_div((argc + 1) as $ty));
+            check!($ty, ($ty::MIN).saturating_div((0 as $ty).wrapping_sub(argc as $ty)));
         };
     }
 
